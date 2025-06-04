dcf9925d904500f472d9bc005782c8fa291848cb
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-06-04 09:41:48 +0800

    Created method for calculating entire group catalogue.

--- src/rust/src/group_properties.rs
+++ src/rust/src/group_properties.rs
@@ -15,6 +15,19 @@ pub struct Group {
 }
 
 impl Group {
+
+    pub fn multiplicity(&self) -> i32 {
+        self.ra_members.len() as i32
+    }
+
+    pub fn median_redshift(&self) -> f64 {
+        median(self.ra_members.iter().copied()).unwrap()
+    }
+
+    pub fn median_distance(&self, cosmo: &Cosmology) -> f64 {
+        cosmo.comoving_distance(self.median_redshift())
+    }
+
     // sigma error would be in km/s. 
     pub fn velocity_dispersion_gapper(&self) -> (f64, f64) {
         let sigma_err_squared = mean(self.velocity_errors.iter().copied());
@@ -39,9 +52,7 @@ impl Group {
         (dispersion, sigma_err_squared.sqrt())
     }
 
-
-
-    pub fn calculate_iterative_center(&self, cosmo: Cosmology) -> (f64, f64) {
+    pub fn calculate_iterative_center(&self, cosmo: &Cosmology) -> (f64, f64) {
     
         let absolute_magnitudes: Vec<f64> = zip(self.magnitudes_members.clone(), self.redshift_members.clone())
             .map(|(mag, z)| mag - cosmo.distance_modulus(z))
@@ -98,7 +109,7 @@ impl Group {
 
 
 
-    pub fn calculate_radius(&self, group_center_ra: f64, group_center_dec: f64, group_center_z: f64, cosmo: Cosmology) -> [f64; 3] {
+    pub fn calculate_radius(&self, group_center_ra: f64, group_center_dec: f64, group_center_z: f64, cosmo: &Cosmology) -> [f64; 3] {
         let group_center_dist= cosmo.comoving_distance(group_center_z);
         let group_dists: Vec<f64> = self.redshift_members.iter().map(|z| cosmo.comoving_distance(*z)).collect();
         let center = convert_equitorial_to_cartesian_scaled(group_center_ra, group_center_dec, group_center_dist);
@@ -125,7 +136,98 @@ impl Group {
 
 }
 
+pub struct GroupedGalaxyCatalog {
+    pub ra: Vec<f64>,
+    pub dec: Vec<f64>,
+    pub redshift: Vec<f64>,
+    pub magnitudes: Vec<f64>,
+    pub velocity_errors: Vec<f64>,
+    pub group_ids: Vec<i32>
+}
 
+impl GroupedGalaxyCatalog {
+    fn get_unique_ids(&self) -> Vec<i32> {
+        let mut ids = self.group_ids.clone();
+        ids.sort();
+        ids.dedup();
+        ids
+    }
+
+    pub fn calculate_group_properties(&self, cosmo: &Cosmology) -> GroupCatalog {
+        let unique_group_ids = self.get_unique_ids();
+        let mut id_groups: Vec<i32> = Vec::new();
+        let mut itercen_ra_groups: Vec<f64> = Vec::new();
+        let mut itercen_dec_groups: Vec<f64> = Vec::new();
+        let mut redshift_groups: Vec<f64> = Vec::new();
+        let mut distance_groups: Vec<f64> = Vec::new();
+        let mut r50_groups: Vec<f64> = Vec::new();
+        let mut r100_groups: Vec<f64> = Vec::new();
+        let mut rsigma_groups: Vec<f64> = Vec::new();
+
+
+        for id in unique_group_ids {
+            if id >= 0 { // ignoring singletons = -1
+                let local_group_ids: Vec<i32> = self.group_ids
+                    .clone()
+                    .into_iter()
+                    .filter(|i| *i == id)
+                    .collect();
+
+                let local_ra: Vec<f64> = local_group_ids.clone().into_iter().map(|i| *self.ra.get(i as usize).unwrap()).collect();
+                let local_dec: Vec<f64> = local_group_ids.clone().into_iter().map(|i| *self.dec.get(i as usize).unwrap()).collect();
+                let local_z: Vec<f64> = local_group_ids.clone().into_iter().map(|i| *self.redshift.get(i as usize).unwrap()).collect();
+                let local_mag: Vec<f64> = local_group_ids.clone().into_iter().map(|i| *self.magnitudes.get(i as usize).unwrap()).collect();
+                let local_vel_errors: Vec<f64> = local_group_ids.clone().into_iter().map(|i| *self.velocity_errors.get(i as usize).unwrap()).collect();
+
+                let local_group = Group {
+                    ra_members: local_ra,
+                    dec_members: local_dec,
+                    redshift_members: local_z,
+                    magnitudes_members: local_mag,
+                    velocity_errors: local_vel_errors
+                };
+
+                let (ra_group, dec_group) = local_group.calculate_iterative_center(cosmo);
+                let z_group = local_group.median_redshift();
+                let distance_group = local_group.median_distance(cosmo);
+                let [r50_group, rsimga_group, r100_group] = local_group.calculate_radius(ra_group, dec_group, z_group, cosmo);
+
+                id_groups.push(id);
+                itercen_ra_groups.push(ra_group);
+                itercen_dec_groups.push(dec_group);
+                redshift_groups.push(z_group);
+                r50_groups.push(r50_group);
+                r100_groups.push(r100_group);
+                rsigma_groups.push(rsimga_group);
+                distance_groups.push(distance_group);
+
+
+            }
+        }
+
+        GroupCatalog {
+            ids: id_groups,
+            ras: itercen_ra_groups,
+            decs: itercen_dec_groups,
+            redshifts: redshift_groups,
+            distances: distance_groups,
+            r50s: r50_groups,
+            r100s: r100_groups,
+            rsigmas: rsigma_groups
+        }
+    }
+}
+
+pub struct GroupCatalog {
+    pub ids: Vec<i32>,
+    pub ras: Vec<f64>,
+    pub decs: Vec<f64>,
+    pub redshifts: Vec<f64>,
+    pub distances: Vec<f64>,
+    pub r50s: Vec<f64>,
+    pub r100s: Vec<f64>,
+    pub rsigmas: Vec<f64>
+}
 
 #[cfg(test)]
 mod tests {
@@ -165,7 +267,7 @@ mod tests {
             h0: 0.7
         };
 
-        let (ra, dec) = group.calculate_iterative_center(cosmo);
+        let (ra, dec) = group.calculate_iterative_center(&cosmo);
         // RA should be close to 180.0 and Dec to 0.0 (within ~0.01 deg)
         assert!((ra - 180.0).abs() < 1e-6, "RA deviated too far: {}", ra);
         assert!(dec.abs() < 1e-6, "Dec deviated too far: {}", dec);
@@ -194,7 +296,7 @@ mod tests {
         let group_z = 0.2;
 
         let answer =  [350.5738627578198, 424.34275593189506, 1312.1178128946078];
-        let result = group.calculate_radius(group_ra, group_dec, group_z, cosmo);
+        let result = group.calculate_radius(group_ra, group_dec, group_z, &cosmo);
         for (r, a) in zip(result, answer) {
             assert!((r - a).abs() < 1e-6)
         }
